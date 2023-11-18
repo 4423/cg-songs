d714f37e48d395b8276defc6c6a29d42f420c311
4423 <hanamiya4423@gmail.com>
2023-11-19 01:25:58 +0900

    Implement partial match mode

--- src/components/Setlist.vue
+++ src/components/Setlist.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="setlist">
-    <Songs
-      v-if="hasArtists && hasSongs"
-      :songs="songs"
-      :selected="artists"
-    ></Songs>
-    <span v-else-if="!hasArtists"> キャラクター名を入力してください。 </span>
-    <span v-else-if="songs == null || songs.length == 0">
+    <div v-if="showPartialMatchButton" class="settings">
+      <b-form-checkbox v-model="enablePartialMatch" name="check-button" switch>
+        部分一致（セトリ予想モード）
+      </b-form-checkbox>
+    </div>
+
+    <span v-if="!hasArtists"> キャラクター名を入力してください。 </span>
+    <span v-else-if="!hasSongs">
       見つかりませんでした。 フルネームで指定していますか？
     </span>
     <span v-else-if="failedToLoad">
       データの読み込みに失敗しました。再度時間を置いて試してください。
     </span>
+
+    <Songs
+      v-if="hasArtists && hasSongs"
+      :songs="songs"
+      :selected="artists"
+    ></Songs>
   </div>
 </template>
 
@@ -28,9 +35,9 @@ export default {
   },
   data: function () {
     return {
-      songs: [],
       allSongs: [],
       failedToLoad: false,
+      enablePartialMatch: false,
     }
   },
   computed: {
@@ -40,11 +47,56 @@ export default {
     hasSongs: function () {
       return this.songs != null && this.songs.length > 0
     },
-  },
-  methods: {
-    getSongsByArtists: function (artists) {
-      return this.allSongs.filter((s) => {
-        return artists.every((a) => s.artists.includes(a))
+    hasSongsByPartialMatch: function () {
+      return (
+        this.songsByPartialMatch != null && this.songsByPartialMatch.length > 0
+      )
+    },
+    showPartialMatchButton: function () {
+      return (
+        this.hasSongsByPartialMatch &&
+        this.artists != null &&
+        this.artists.length >= 2
+      )
+    },
+    songs: function () {
+      if (this.enablePartialMatch) {
+        return this.songsByPartialMatch
+      } else {
+        return this.songsByPartialMatch.filter((s) => {
+          return this.artists.every((a) => s.artists.includes(a))
+        })
+      }
+    },
+    songsByPartialMatch: function () {
+      // 一致度を表すプロパティを追加
+      const songsWithScore = []
+      this.allSongs.forEach((s) => {
+        const matchedCount = this.artists.filter((a) =>
+          s.artists.includes(a)
+        ).length
+        s["matchedCount"] = matchedCount
+        s["score"] = matchedCount / s.artists.length
+        if (matchedCount > 0) {
+          songsWithScore.push(s)
+        }
+      })
+      // 次の優先度でソート
+      // 1. matchedCount の降順
+      // 2. score の降順
+      // 3. artists 数の昇順
+      return songsWithScore.sort((a, b) => {
+        if (a.matchedCount === b.matchedCount) {
+          if (a.score === b.score) {
+            if (a.artists.length > b.artists.length) {
+              return -1
+            } else if (a.artists.length < b.artists.length) {
+              return 1
+            }
+          }
+          return b.score - a.score
+        }
+        return b.matchedCount - a.matchedCount
       })
     },
   },
@@ -55,17 +107,18 @@ export default {
       )
       .then((response) => {
         this.allSongs = response.data
-        this.songs = this.getSongsByArtists(this.artists)
         this.failedToLoad = false
       })
       .catch(() => {
         this.failedToLoad = true
       })
   },
-  watch: {
-    artists: function () {
-      this.songs = this.getSongsByArtists(this.artists)
-    },
-  },
 }
 </script>
+
+<style scoped>
+.settings {
+  text-align: right;
+  margin: 2rem 0;
+}
+</style>
